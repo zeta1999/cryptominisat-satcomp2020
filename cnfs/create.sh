75ee95259c7ca9232fca161cfae8e50506790587
Mate Soos <soos.mate@gmail.com>
2020-04-11 11:55:34 +0200

    Adding CNF updates

--- cnfs/create.sh
+++ cnfs/create.sh
@@ -1,11 +1,48 @@
-./makewff -seed 2121884153  -cnf 3 400 1700 > f400.cnf
-./multipart.py f400.cnf mizh-md5-47-3.cnf >out.cnf
-./walksat f400.cnf
+#!/usr/bin/bash
 
-echo "700"
+set -x
 
-num=$(python -c 'print("%d" % (4.25*700))')
-./makewff -seed 21884153  -cnf 3 700 ${num} > f700.cnf
-./multipart.py f700.cnf mizh-md5-47-3.cnf >out.cnf
-./walksat f700 -> 0.23s
+base=600
+echo "Doing $base"
+num=$(python -c "print('%d' % (4.25*${base}))")
+./makewff -seed 29988413  -cnf 3 ${base} ${num} > f$base.cnf
+walksat f$base.cnf
+ccanr -inst f$base.cnf
+(cd ~/development/grainofsalt/build/
+rm satfiles/*
+./grainofsalt --seed 0 --crypto crypto1 --outputs 50 --karnaugh 0 --init no --num 1 > out
+file=$(grep "File output" out | awk '{print $3}')
+echo "crypto file was: $file"
+)
+cp ~/development/grainofsalt/build/satfiles/$file .
+./multipart.py $file f$base.cnf > out4.cnf
 
+base=650
+echo "Doing $base"
+num=$(python -c "print('%d' % (4.25*${base}))")
+./makewff -seed 21978423  -cnf 3 ${base} ${num} > f$base.cnf
+walksat f$base.cnf
+ccanr -inst f$base.cnf
+(cd ~/development/grainofsalt/build/
+rm satfiles/*
+./grainofsalt --seed 1 --crypto crypto1 --outputs 50 --karnaugh 0 --init no --num 1 > out
+file=$(grep "File output" out | awk '{print $3}')
+echo "crypto file was: $file"
+)
+cp ~/development/grainofsalt/build/satfiles/$file .
+./multipart.py $file f$base.cnf > out5.cnf
+
+base=700
+echo "Doing $base"
+num=$(python -c "print('%d' % (4.25*${base}))")
+./makewff -seed 31978313  -cnf 3 ${base} ${num} > f$base.cnf
+walksat f$base.cnf
+ccanr -inst f$base.cnf
+(cd ~/development/grainofsalt/build/
+rm satfiles/*
+./grainofsalt --seed 2 --crypto crypto1 --outputs 50 --karnaugh 0 --init no --num 1 > out
+file=$(grep "File output" out | awk '{print $3}')
+echo "crypto file was: $file"
+)
+cp ~/development/grainofsalt/build/satfiles/$file .
+./multipart.py $file f$base.cnf > out6.cnf
