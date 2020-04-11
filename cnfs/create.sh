f3061594e35dd1a911fd688cda8c27b0c4a007b5
Mate Soos <soos.mate@gmail.com>
2020-04-11 13:46:18 +0200

    Updating creator

--- cnfs/create.sh
+++ cnfs/create.sh
@@ -5,59 +5,12 @@ set -x
 rm makewff
 gcc makewff.c -o makewff
 
-base=600
-echo "Doing $base"
-num=$(python -c "print('%d' % (4.25*${base}))")
-./makewff -seed 29988413  -cnf 3 ${base} ${num} > f$base.cnf
-walksat f$base.cnf
-ccanr -inst f$base.cnf > solution-f$base.cnf
-(cd ~/development/grainofsalt/build/
-rm satfiles/*
-./grainofsalt --seed 0 --crypto crypto1 --outputs 50 --karnaugh 0 --init no --num 1 > out
-)
-file=$(grep "File output" ~/development/grainofsalt/build/out | awk '{print $3}')
-echo "crypto file was: $file"
-cadical $file > solution-$file
-cp ~/development/grainofsalt/build/satfiles/$file .
-./multipart.py $file f$base.cnf > out4.cnf
-
-base=650
-echo "Doing $base"
-num=$(python -c "print('%d' % (4.25*${base}))")
-./makewff -seed 21978423  -cnf 3 ${base} ${num} > f$base.cnf
-walksat f$base.cnf
-ccanr -inst f$base.cnf
-(cd ~/development/grainofsalt/build/
-rm satfiles/*
-./grainofsalt --seed 1 --crypto crypto1 --outputs 50 --karnaugh 0 --init no --num 1 > out
-)
-file=$(grep "File output" ~/development/grainofsalt/build/out | awk '{print $3}')
-echo "crypto file was: $file"
-cp ~/development/grainofsalt/build/satfiles/$file .
-./multipart.py $file f$base.cnf > out5.cnf
-
-base=700
-echo "Doing $base"
-num=$(python -c "print('%d' % (4.25*${base}))")
-./makewff -seed 31978313  -cnf 3 ${base} ${num} > f$base.cnf
-walksat f$base.cnf
-ccanr -inst f$base.cnf
-(cd ~/development/grainofsalt/build/
-rm satfiles/*
-./grainofsalt --seed 2 --crypto crypto1 --outputs 50 --karnaugh 0 --init no --num 1 > out
-)
-file=$(grep "File output" ~/development/grainofsalt/build/out | awk '{print $3}')
-echo "crypto file was: $file"
-cp ~/development/grainofsalt/build/satfiles/$file .
-./multipart.py $file f$base.cnf > out6.cnf
-
-# ---------
+base=450
+cryptocomplex=45
 
 lastseed=0
 for number_of_problems in {0..20};
 do
-    base=450
-    cryptocomplex=45
     echo "Doing $base"
     num=$(python -c "print('%d' % (4.25*${base}))")
     for i in {${lastseed}..1000}
@@ -85,8 +38,8 @@ do
     cadical $fnamecdcl > solution-$fnamecdcl
     echo "crypto file was: $fnamecdcl"
     echo "SLS    file was: $fnamesls"
-    finalfile="combined-crypto1-wff-seed-$seed-wffvars-$base-cryptocomplex-$cryptocomplex.cnf"
-    ./multipart-match-sol.py $fnamecdcl $fnamesls solution-$fnamecdcl solution-$fnamesls > $finalfile
+    finalfile="combined-crypto1-wff-seed-$seed-wffvars-$base-cryptocplx-$cryptocomplex.cnf"
+    ./multipart-match-sol.py $fnamecdcl $fnamesls solution-$fnamecdcl solution-$fnamesls 5 > $finalfile
     echo "created $finalfile"
     lastseed=$((i+1))
 done;
