ea38eb2f5c1c868df5f56bff3b865b61c1c72558
Mate Soos <soos.mate@gmail.com>
2020-04-11 13:40:53 +0200

    Updating to newer version

--- cnfs/old_create.sh
+++ cnfs/old_create.sh
@@ -1,10 +1,3 @@
-#!/usr/bin/bash
-
-set -x
-
-rm makewff
-gcc makewff.c -o makewff
-
 base=600
 echo "Doing $base"
 num=$(python -c "print('%d' % (4.25*${base}))")
@@ -50,45 +43,3 @@ file=$(grep "File output" ~/development/grainofsalt/build/out | awk '{print $3}'
 echo "crypto file was: $file"
 cp ~/development/grainofsalt/build/satfiles/$file .
 ./multipart.py $file f$base.cnf > out6.cnf
-
-# ---------
-
-lastseed=0
-for number_of_problems in {0..20};
-do
-    base=450
-    cryptocomplex=45
-    echo "Doing $base"
-    num=$(python -c "print('%d' % (4.25*${base}))")
-    for i in {${lastseed}..1000}
-    do
-        fnamesls="f$base-seed${i}.cnf"
-        ./makewff -seed $i  -cnf 3 ${base} ${num} > $fnamesls
-        (ulimit -t 2
-        ccanr -inst $fnamesls > solution-$fnamesls
-        )
-        x=$(grep "s SATISFIABLE" solution-$fnamesls)
-        echo "x is: $x"
-        if [[ "$x" == *"SATISFIABLE"* ]]; then
-            echo "seed was: $i"
-            break;
-        fi
-        rm -f solution-$fnamesls
-    done;
-    (cd ~/development/grainofsalt/build/
-    rm satfiles/*
-    echo "Using seed: ${i}"
-    ./grainofsalt --seed ${i} --crypto crypto1 --outputs $cryptocomplex --karnaugh 0 --init no --num 1 > out
-    )
-    fnamecdcl=$(grep "File output" ~/development/grainofsalt/build/out | awk '{print $3}')
-    cp ~/development/grainofsalt/build/satfiles/$fnamecdcl .
-    cadical $fnamecdcl > solution-$fnamecdcl
-    echo "crypto file was: $fnamecdcl"
-    echo "SLS    file was: $fnamesls"
-    finalfile="combined-crypto1-wff-seed-$seed-wffvars-$base-cryptocomplex-$cryptocomplex.cnf"
-    ./multipart-match-sol.py $fnamecdcl $fnamesls solution-$fnamecdcl solution-$fnamesls > $finalfile
-    echo "created $finalfile"
-    lastseed=$((i+1))
-done;
-
-
